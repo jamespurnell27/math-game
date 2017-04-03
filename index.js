65d462cd76b07454a2375409e271bba59f9a7f47
James Purnell <jamespurnell27@hotmail.com>
2017-04-03 21:33:20 +1000

    experiment with socket.io chat tutorial

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 var express = require('express');
-app = express();
-var http = require('http').Server(app);
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
-app.use(express.static('public'));
+
+
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected nope');
+  });
+  socket.on('Chat Msg', function (msg){
+    io.emit('Chat Msg', msg);
+  });
+});
+
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
